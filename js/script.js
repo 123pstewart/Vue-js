30f6c4cd8373a67b33a334e28def93f407e94dd0
Preston Stewart <123pstewart@frhsd.com>
2023-02-15 08:27:03 -0500

    Video number 3, toggling

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@ const app = Vue.createApp({
     // template: '<h2> I am the template</h2>'
     data(){
         return{
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
         }
+    },
+    methods: {
+        toggleShowBooks() {
+        this.showBooks = !this.showBooks;
+        }
     }
-    
 });
 
 app.mount('#app')
