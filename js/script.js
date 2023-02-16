4ff9122015d10bdb537a111d2187366c371b31d2
Preston Stewart <123pstewart@frhsd.com>
2023-02-16 09:46:58 -0500

    Day 3, binding

--- js/script.js
+++ js/script.js
@@ -3,16 +3,34 @@ const app = Vue.createApp({
     // template: '<h2> I am the template</h2>'
     data(){
         return{
+            url:'http://www.thenetninja.co.uk',
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            // title: 'The Final Empire',
+            // author: 'Brandon Sanderson',
+            // age: 45,
+            // x: 0,
+            // y: 0,
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss'},
+                { title: 'the way of kings', author: 'brandon sanderson'},
+                { title: 'the final empire', author: 'brandon sanderson'}
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
         this.showBooks = !this.showBooks;
         }
+        // handleEvent(e, data){
+        //     console.log(e, e.type)
+        //     if (data){
+        //         console.log(data)
+        //     }
+        // },
+        // handleMousemove(e) {
+        // this.x = e.offsetX
+        // this.y = e.offsetY
+        // }
     }
 });
 
